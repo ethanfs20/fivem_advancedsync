e9a06502934c4b99bacde385500670d0cd6dfed9
Ethan4t0r <eshearersocial@gmail.com>
2023-08-15 21:56:02 -0400

    update fxmanifest

--- fivem_advancedsync/fxmanifest.lua
+++ fivem_advancedsync/fxmanifest.lua
@@ -1,8 +1,9 @@
 fx_version 'bodacious'
 game 'gta5'
 
-author 'You'
+author 'Ethan4t0r'
 version '1.0.0'
+description 'Advanced sync for FiveM in C#'
 
 fxdk_watch_command 'dotnet' {'watch', '--project', 'Client/fivem_advancedsync.Client.csproj', 'publish', '--configuration', 'Release'}
 fxdk_watch_command 'dotnet' {'watch', '--project', 'Server/fivem_advancedsync.Server.csproj', 'publish', '--configuration', 'Release'}
